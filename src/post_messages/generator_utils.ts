3646ef0e00400d2480d230209c41fcc0bc83a2fd
Marcos Minond <marcos.minond@mx.com>
2022-02-02 01:05:52 -0700

    Auto generating type definitions

---
+++ src/post_messages/generator_utils.ts
@@ -0,0 +1,65 @@
+import { readFileSync, writeFileSync } from "fs"
+import { join } from "path"
+import YAML from "yaml"
+
+YAML.defaultOptions.merge = true
+
+export type NamespaceDefinition = {
+  [index: string]: ActionDefinition
+}
+
+export type ActionDefinition = {
+  [index: string]: MessageDefinition
+}
+
+export type MessageDefinition = {
+  [index: string]: "string" | "number"
+}
+
+export const withEachMessageDefinition = <R>(cb: (ns: string, action: string, defn: MessageDefinition) => R): R[] => {
+  const namespaces = loadNamespaces()
+  const results: R[] = []
+
+  for (const namespace in namespaces) {
+    const actions = namespaces[namespace]
+    for (const action in actions) {
+      results.push(cb(namespace, action, actions[action]))
+    }
+  }
+
+  return results
+}
+
+export const loadNamespaces = (): NamespaceDefinition => {
+  const defsFile = join(__dirname, "./event_definitions.yml")
+  console.log(`Loading post message event definitions from ${defsFile}`)
+
+  const defsData = readFileSync(defsFile)
+  const data =  YAML.parse(defsData.toString()) as { namespaces: NamespaceDefinition }
+  return data.namespaces
+}
+
+export const write = (dest: string, contents: string) => {
+  console.log("Writing %d bytes to %s", contents.length, dest)
+  writeFileSync(dest, contents)
+}
+
+export const merge = (template: string, fields: Record<string, string>) =>
+  Object.keys(fields).reduce((str, field) =>
+    str.replace(`{${field}}`, fields[field]), template)
+
+export const genMessageKey = (namespace: string, action: string) =>
+  camelCase(isParentDefn(action) ? namespace : `${namespace}_${action}`)
+
+export const genMessageType =(namespace: string, action: string) =>
+  isParentDefn(action) ? `mx/${namespace}` : `mx/${namespace}/${action}`
+
+export const isParentDefn = (action: string) =>
+  action === "_"
+
+export const camelCase = (str: string) =>
+  str
+    .replace(/(^[a-z])/i, (match) => match.toUpperCase())
+    .replace(/([-_][a-z])/ig, (match) => match.toUpperCase())
+    .replace("-", "")
+    .replace("_", "")
--- src/post_messages/type.ts
+++ src/post_messages/type.ts
@@ -1,15 +1,1 @@
-export enum Type {
-  Load = "mx/load",
-
-  ConnectLoaded = "mx/connect/loaded",
-  ConnectSelectedInstitution = "mx/connect/selectedInstitution",
-  ConnectStepChange = "mx/connect/stepChange",
-}
-
-export const typeMap: Record<string, Type> = {
-  [Type.Load]: Type.Load,
-
-  [Type.ConnectLoaded]: Type.ConnectLoaded,
-  [Type.ConnectSelectedInstitution]: Type.ConnectSelectedInstitution,
-  [Type.ConnectStepChange]: Type.ConnectStepChange,
-}
+export * from "./generated_type"
