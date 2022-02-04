5b0c73f0378550257fdd73b37844692d06aa9f30
Marcos Minond <marcos.minond@mx.com>
2022-02-03 23:36:06 -0700

    Switching how configuration is loaded so there is no need to link a native package

--- App.tsx
+++ App.tsx
@@ -1,16 +1,36 @@
 import React from "react"
 import { View } from "react-native"
-import Config from "react-native-config"
+
+import config from "./config.json"
 import ConnectWidget from "./src/components/ConnectWidget"
+
+const clientId = config["MX_CLIENT_ID"]
+const apiKey = config["MX_API_KEY"]
+const userGuid = config["MX_USER_GUID"]
+const environment = config["MX_ENVIRONMENT"]
+
+if (!clientId || !apiKey || !userGuid || !environment) {
+  const message = `
+    Missing configuration. I need the following values defined in config.json:
+
+      - MX_CLIENT_ID
+      - MX_API_KEY
+      - MX_USER_GUID
+      - MX_ENVIRONMENT
+
+    See README for instructions.`
+
+  throw new Error(message)
+}
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
       <ConnectWidget
-        clientId={Config.MX_CLIENT_ID}
-        apiKey={Config.MX_API_KEY}
-        userGuid={Config.MX_USER_GUID}
-        environment={Config.MX_ENVIRONMENT}
+        clientId={clientId}
+        apiKey={apiKey}
+        userGuid={userGuid}
+        environment={environment}
         onLoad={(payload) => {
           console.log("Widget is loading")
         }}
