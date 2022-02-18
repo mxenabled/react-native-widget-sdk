4abe2731043ecedab0dcf77389101c3ca66e0984
Marcos Minond <marcos.minond@mx.com>
2022-02-17 23:25:30 -0700

    Budgets widget test wip

---
+++ test/components/BudgetsWidget_test.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { render, waitFor } from '@testing-library/react-native';
+
+import { BudgetsWidget } from "../../src/components/BudgetsWidget"
+
+describe("BudgetsWidget", () => {
+  describe("widget loading", () => {
+    test("it is able to load widget when Platform API props are passed in", async () => {
+      const component = render(
+        <BudgetsWidget
+          clientId="myveryownclientid"
+          apiKey="myveryownapikey"
+          userGuid="USR-777"
+          environment="integration"
+        />
+      )
+
+      const webView = await waitFor(() => component.findByTestId("budgets_widget_webview"))
+      expect(webView.props.source.uri).toContain("https://int-widgets.moneydesktop.com/md/budgets/")
+    })
+  })
+})
