042134621a97e9410912d16977ae32f27571943a
Marcos Minond <marcos.minond@mx.com>
2022-03-02 15:12:40 -0700

    Adding linting rule for double quotes and semicolons

--- test/components/MoneyMapWidgets_test.tsx
+++ test/components/MoneyMapWidgets_test.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { render, waitFor } from '@testing-library/react-native';
+import { render, waitFor } from "@testing-library/react-native"
 
 import { BudgetsWidget } from "../../src/components/MoneyMapWidgets"
 
