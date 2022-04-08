2f9ab11ed2fbb69b863bf9824b3ed3f68ea04093
Marcos Minond <marcos.minond@mx.com>
2022-04-08 11:04:37 -0600

    Fixing example app

--- example/App.tsx
+++ example/App.tsx
@@ -1,13 +1,13 @@
 import React from "react"
 import { View } from "react-native"
 
-import { ConnectAggregationWidget } from "@mxenabled/react-native-widget-sdk"
+import { ConnectWidget } from "@mxenabled/react-native-widget-sdk"
 import config from "./config.json"
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
-      <ConnectAggregationWidget
+      <ConnectWidget
         {...config}
 
         onSsoUrlLoadError={(error) => {
