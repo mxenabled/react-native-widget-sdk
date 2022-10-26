55e680858d7295089f2831c4e30a7196b159d0de
Marcos Minond <marcos.minond@mx.com>
2022-10-25 20:30:27 -0600

    Removing comments

--- src/react_native_v64_compat.ts
+++ src/react_native_v64_compat.ts
@@ -7,15 +7,3 @@ export type RNv64Compat<T> = T & {
   removeEventListener(type: "url", handler: unknown): void
   removeEventListener(type: "change", handler: unknown): void
 }
-
-// import "react-native"
-//
-// declare module "react-native" {
-//   export interface LinkingStatic {
-//     removeEventListener(type: "url", handler: unknown): void
-//   }
-//
-//   export interface Dimensions {
-//     removeEventListener(type: "change", handler: unknown): void
-//   }
-// }
