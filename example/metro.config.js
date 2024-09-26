ed7c7e7f9be17b5eb112a4c81279122309b2c44e
Clement Mwimo <clement.mwimo@mx.com>
2024-09-26 17:26:55 -0600

    upgrade ios app to react-native 0.75.3

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,14 +1,14 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
-const { resolve } = require('path')
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config")
+const { resolve } = require("path")
 
 const siblings = {
-  '@mxenabled/react-native-widget-sdk': resolve(__dirname, '..', 'src'),
-  '@mxenabled/widget-post-message-definitions': resolve(
+  "@mxenabled/react-native-widget-sdk": resolve(__dirname, "..", "src"),
+  "@mxenabled/widget-post-message-definitions": resolve(
     __dirname,
-    '..',
-    'node_modules',
-    '@mxenabled',
-    'widget-post-message-definitions',
+    "..",
+    "node_modules",
+    "@mxenabled",
+    "widget-post-message-definitions",
   ),
 }
 /**
@@ -31,7 +31,7 @@ const config = {
       {},
       {
         get: (target, name) =>
-          name in siblings ? siblings[name] : resolve(process.cwd(), 'node_modules', name),
+          name in siblings ? siblings[name] : resolve(process.cwd(), "node_modules", name),
       },
     ),
   },
